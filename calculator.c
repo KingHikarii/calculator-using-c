76677bed7d2bedb2aacccd6ecb60cf234f3380e1
KingHikarii <103539937+KingHikarii@users.noreply.github.com>
2022-05-30 16:01:14 +0100

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void main()
 {
@@ -10,22 +11,75 @@ void main()
     printf("Press (*) for Multiplication \n");
     printf("Press (/) for Division \n");
     printf("Press (^) for Exponential \n");
-    printf("Type (sqrt) to Square Root \n");
-    printf("Type (exit) for exit \n");
+    printf("Type s to Square Root \n");
+    printf("Type e for exit \n");
 
-    char operation[4];
-    char *sqrt = "sqrt";
 
-    gets(operation);
-    //printf(sqrt);
+    int a, b, result;
+        char selection;
 
-    while (strcmp(sqrt,operation) != 0) {
-        printf("You selected sqrt!");
-        break;
+    printf("Enter a number: ");
+    scanf(" %d", &a);
+
+    printf("Enter another number: ");
+    scanf(" %d", &b);
+
+    printf("Enter a selection: ");
+    scanf(" %c", &selection);
+
+
+    switch(selection) {
+
+    case '+':
+    result = a + b;
+    printf("%d + %d = %d", a,b ,result);
+    break;
+
+    case '-':
+    result = a - b;
+    printf("%d + %d = %d", a,b ,result);
+    break;
+
+    case '*':
+    result = a * b;
+    printf("%d + %d = %d", a,b ,result);
+    break;
+
+    case '/':
+    result = a / b;
+    printf("%d + %d = %d", a,b ,result);
+    break;
+
+    case '^':
+    result = double pow(double a, double b);
+    printf("%f of %f is %f", a,b ,result);
+    break;
+
+    case 'e':
+    exit(0);
+
+    case 's':
+    result = sqrt(a);
+    printf("The square root is: %d", result);
+
     }
 
 
+
+
+
+
+ //printf("The result is %d",result);
+
+    while (1) {
+        if(strcmp(sqrt,selection) == 0){
+        printf("MATCHED!");
+       break;
+
+
     }
+    }
+}
 
 
 
